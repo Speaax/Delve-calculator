2fffb62f28813e49b1536f5d05c259f235664af7
speaax <speaax@gmail.com>
2025-10-19 14:11:29 +0200

    Added settings to only show side panel when near delve or reading scoreboard.

--- src/main/java/com/speaax/DelveCalculatorConfig.java
+++ src/main/java/com/speaax/DelveCalculatorConfig.java
@@ -3,6 +3,7 @@ package com.speaax;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup("delvecalculator")
 public interface DelveCalculatorConfig extends Config
@@ -23,4 +24,42 @@ public interface DelveCalculatorConfig extends Config
             description = ""
     )
     void killCountData(String data);
+
+    @ConfigSection(
+            name = "Panel Settings",
+            description = "Configure when and how the side panel is displayed",
+            position = 0
+    )
+    String panelSettings = "panelSettings";
+
+    enum PanelDisplayMode
+    {
+        REGION,
+        SCOREBOARD,
+        ALWAYS
+    }
+
+    @ConfigItem(
+            keyName = "displayPanel",
+            name = "Display Panel",
+            description = "Configures when the side panel should be visible.",
+            section = panelSettings,
+            position = 1
+    )
+    default PanelDisplayMode displayPanel()
+    {
+        return PanelDisplayMode.ALWAYS;
+    }
+
+    @ConfigItem(
+            keyName = "autoOpenPanel",
+            name = "Auto Open Panel",
+            description = "Automatically open the side panel when the display condition is met. This does not apply to 'Always'.",
+            section = panelSettings,
+            position = 2
+    )
+    default boolean autoOpenPanel()
+    {
+        return true;
+    }
 }
--- src/main/java/com/speaax/DelveCalculatorPlugin.java
+++ src/main/java/com/speaax/DelveCalculatorPlugin.java
@@ -5,11 +5,15 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
+import net.runelite.api.GameState;
 import net.runelite.api.events.ChatMessage;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.WidgetClosed;
 import net.runelite.api.events.WidgetLoaded;
 import net.runelite.api.widgets.Widget;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
@@ -20,7 +24,9 @@ import net.runelite.client.config.ConfigManager;
 import com.google.inject.Provides;
 
 import javax.inject.Inject;
+import javax.swing.*;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -53,53 +59,31 @@ public class DelveCalculatorPlugin extends Plugin
 
 	private DelveCalculatorPanel panel;
 	private NavigationButton navButton;
+	private boolean panelVisible = false;
 
 	// Widget group ID
 	private static final int WIDGET_GROUP = 920;
+
+
+	private static final int[] DELVE_REGION_IDS = {
+			5269,
+			13668,
+			14180
+	};
 
 	// Drop rate data based on the table you provided
 	private static final Map<Integer, DropRates> DROP_RATES_BY_LEVEL = new HashMap<>();
 
 	static {
-		// Delve level 2
-		DROP_RATES_BY_LEVEL.put(2, new DropRates(
-			1.0/2500, 1.0/2500, 0, 0, 0, 0
-		));
-		
-		// Delve level 3
-		DROP_RATES_BY_LEVEL.put(3, new DropRates(
-			1.0/1000, 1.0/2000, 1.0/2000, 0, 0, 0
-		));
-		
-		// Delve level 4
-		DROP_RATES_BY_LEVEL.put(4, new DropRates(
-			1.0/450, 1.0/1350, 1.0/1350, 1.0/1350, 0, 0
-		));
-		
-		// Delve level 5
-		DROP_RATES_BY_LEVEL.put(5, new DropRates(
-			1.0/270, 1.0/810, 1.0/810, 1.0/810, 0, 0
-		));
-		
-		// Delve level 6
-		DROP_RATES_BY_LEVEL.put(6, new DropRates(
-			1.0/255, 1.0/765, 1.0/765, 1.0/765, 1.0/1000, 0
-		));
-		
-		// Delve level 7
-		DROP_RATES_BY_LEVEL.put(7, new DropRates(
-			1.0/240, 1.0/720, 1.0/720, 1.0/720, 1.0/750, 0
-		));
-		
-		// Delve level 8
-		DROP_RATES_BY_LEVEL.put(8, new DropRates(
-			1.0/210, 1.0/630, 1.0/630, 1.0/630, 1.0/500, 0
-		));
-		
-		// Delve level 9+
-		DROP_RATES_BY_LEVEL.put(9, new DropRates(
-			1.0/180, 1.0/540, 1.0/540, 1.0/540, 1.0/250, 0
-		));
+		//DROP_RATES_BY_LEVEL.put(x... where x is delve level and 9 is everything from 9 and above
+		DROP_RATES_BY_LEVEL.put(2, new DropRates(1.0/2500, 1.0/2500, 0, 0, 0, 0));
+		DROP_RATES_BY_LEVEL.put(3, new DropRates(1.0/1000, 1.0/2000, 1.0/2000, 0, 0, 0));
+		DROP_RATES_BY_LEVEL.put(4, new DropRates(1.0/450, 1.0/1350, 1.0/1350, 1.0/1350, 0, 0));
+		DROP_RATES_BY_LEVEL.put(5, new DropRates(1.0/270, 1.0/810, 1.0/810, 1.0/810, 0, 0));
+		DROP_RATES_BY_LEVEL.put(6, new DropRates(1.0/255, 1.0/765, 1.0/765, 1.0/765, 1.0/1000, 0));
+		DROP_RATES_BY_LEVEL.put(7, new DropRates(1.0/240, 1.0/720, 1.0/720, 1.0/720, 1.0/750, 0));
+		DROP_RATES_BY_LEVEL.put(8, new DropRates(1.0/210, 1.0/630, 1.0/630, 1.0/630, 1.0/500, 0));
+		DROP_RATES_BY_LEVEL.put(9, new DropRates(1.0/180, 1.0/540, 1.0/540, 1.0/540, 1.0/250, 0));
 	}
 
 	@Provides
@@ -131,8 +115,10 @@ public class DelveCalculatorPlugin extends Plugin
 			.build();
 		
 		clientToolbar.addNavigation(navButton);
-		
 
+		panelVisible = true;
+
+		updatePanelVisibility();
 	}
 
 	@Override
@@ -199,14 +185,128 @@ public class DelveCalculatorPlugin extends Plugin
 	}
 
 	@Subscribe
+	public void onConfigChanged(ConfigChanged event)
+	{
+		if (event.getGroup().equals("delvecalculator") && event.getKey().equals("displayPanel"))
+		{
+			updatePanelVisibility();
+		}
+	}
+
+	@Subscribe
 	public void onWidgetLoaded(WidgetLoaded event)
 	{
 		if (event.getGroupId() == WIDGET_GROUP)
 		{
-			// Use ClientThread instead of Timer for safer execution
 			clientThread.invokeLater(this::updateKillCounts);
+			if (config.displayPanel() == DelveCalculatorConfig.PanelDisplayMode.SCOREBOARD)
+			{
+				togglePanel(true, config.autoOpenPanel());
+			}
 		}
 	}
+
+	@Subscribe
+	public void onWidgetClosed(WidgetClosed event)
+	{
+		if (event.getGroupId() == WIDGET_GROUP)
+		{
+			if (config.displayPanel() == DelveCalculatorConfig.PanelDisplayMode.SCOREBOARD)
+			{
+				togglePanel(false, false);
+			}
+		}
+	}
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	{
+		GameState state = gameStateChanged.getGameState();
+		if (state == GameState.LOGGED_IN)
+		{
+			// This handles the login/re-login scenario to ensure the panel visibility is correct.
+			updatePanelVisibility();
+		}
+		else if (state == GameState.LOADING)
+		{
+			// This handles moving between map regions (teleporting, running).
+			clientThread.invokeLater(this::updatePanelVisibilityForRegion);
+		}
+	}
+
+	private void updatePanelVisibilityForRegion()
+	{
+		if (config.displayPanel() == DelveCalculatorConfig.PanelDisplayMode.REGION)
+		{
+			boolean shouldBeVisible = isInDelveRegion();
+			togglePanel(shouldBeVisible, shouldBeVisible && config.autoOpenPanel());
+		}
+	}
+
+	private void updatePanelVisibility()
+	{
+		boolean shouldBeVisible;
+		switch (config.displayPanel())
+		{
+			case ALWAYS:
+				shouldBeVisible = true;
+				break;
+			case REGION:
+				shouldBeVisible = isInDelveRegion();
+				break;
+			case SCOREBOARD:
+				shouldBeVisible = isScoreboardVisible();
+				break;
+			default:
+				shouldBeVisible = false;
+				break;
+		}
+		togglePanel(shouldBeVisible, false); // Don't auto-open on config change/startup
+	}
+
+	private boolean isInDelveRegion()
+	{
+		if (client.getGameState() != GameState.LOGGED_IN)
+		{
+			return false;
+		}
+		// Checks if any of the currently loaded map regions are in our list of Delve regions.
+		return Arrays.stream(client.getMapRegions()).anyMatch(
+				regionId -> Arrays.stream(DELVE_REGION_IDS).anyMatch(delveId -> delveId == regionId)
+		);
+	}
+
+	private boolean isScoreboardVisible()
+	{
+		Widget scoreboardWidget = client.getWidget(WIDGET_GROUP, 0);
+		return scoreboardWidget != null && !scoreboardWidget.isHidden();
+	}
+
+	private void togglePanel(boolean show, boolean autoOpen)
+	{
+		SwingUtilities.invokeLater(() -> {
+			if (show)
+			{
+				if (!panelVisible)
+				{
+					clientToolbar.addNavigation(navButton);
+					panelVisible = true;
+				}
+
+				if (autoOpen)
+				{
+					SwingUtilities.invokeLater(() -> clientToolbar.openPanel(navButton));
+				}
+			}
+			else
+			{
+				// This is the key fix: always remove the button if it should be hidden,
+				// regardless of what our boolean thought its state was.
+				clientToolbar.removeNavigation(navButton);
+				panelVisible = false;
+			}
+		});
+	}
+
 
 	// Static method to get drop rates for external access
 	public static Map<Integer, DropRates> getDropRates()
